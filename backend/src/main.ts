81773056bc26f84146b4a091facd382e21ed2429
AngelNovelli <angeljesusnovelli@gmail.com>
2026-07-04 22:46:53 -0300

    new main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -48,6 +48,8 @@ async function bootstrap() {
     }
   );
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0'); 
+  console.log(`🚀 Aplicación corriendo en el puerto: ${port}`);
 }
 bootstrap();
