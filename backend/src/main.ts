4349f455e809d8430479c1704a2974cd412d193b
AngelNovelli <angeljesusnovelli@gmail.com>
2026-07-04 22:44:41 -0300

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   app.use(urlencoded({ extended: true, limit: '500mb' }));
 
   const allowedOrigins = [
+    'https://catube.up.railway.app',
     'https://catube-steel.vercel.app',
     'https://catube.xyz',
     'https://www.catube.xyz',
@@ -47,6 +48,6 @@ async function bootstrap() {
     }
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
